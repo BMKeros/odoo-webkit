0b879ad74833fde4c1776b644994439701a11fea
Israel Lugo <hostelixisrael@gmail.com>
2018-04-13 15:37:21 -0400

    [ADD] add module auth

---
+++ src/auth.js
@@ -0,0 +1,17 @@
+import Rpc from './rpc';
+
+class Auth extends Rpc {
+    constructor(...args) {
+        super(...args);
+    }
+
+    login(login, password, db = '') {
+        return super.rpc("/web/session/authenticate", {login, password, db});
+    }
+
+    logout() {
+        return super.rpc("/web/session/destroy", {});
+    }
+}
+
+export default Auth;

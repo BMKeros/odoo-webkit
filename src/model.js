1abe1984cb61bdf8441f1fea81b2085050a931be
Israel Lugo <hostelixisrael@gmail.com>
2018-04-16 15:07:19 -0400

    [MOD] add new method to class model

--- src/model.js
+++ src/model.js
@@ -3,12 +3,55 @@ import BaseModel from './base_model';
 class Model extends BaseModel {
     constructor(...args){
         super(...args);
+
+        this._filter = [];
+        this._context = {};
     }
 
+    /**
+     * Set filter model
+     * @param {Array} filters
+     */
+    set filter(filters) {
+        this._filter = filters;
+    }
+
+    /**
+     * Get filter model
+     *
+     * @returns {Array} [filter]
+     */
+    get filter() {
+        return this._filter;
+    }
+
+    /**
+     * Get context model
+     *
+     * @returns {Object}
+     */
+    get_context() {
+        return this._context;
+    }
+
+    /**
+     * ----------
+     *
+     * @returns {Primise}
+     */
     call(...args){
         return super.call(...args);
     }
-    
+
+    /**
+     * ----------
+     *
+     * @returns {Promise}
+     */
+    call_button(...args) {
+        return super.call_button(...args);
+    }
+
     /**
      * Creates a new record in db
      *
@@ -19,7 +62,7 @@ class Model extends BaseModel {
         return super.call({
             method: 'create',
             args: [data],
-            kwargs: { context: {}}
+            kwargs: {context: this.get_context()}
         });
     }
 
@@ -28,13 +71,14 @@ class Model extends BaseModel {
      *
      * @param {Number|String} id identifier for the record to alter
      * @param {Object} data field values to write into the record
+     * @param {Object} options
      * @returns {Promise}
      */
     write(id, data, options = {}) {
         return super.call({
             method: 'write',
             args: [[id], data],
-            kwargs: { context: {}}
+            kwargs: {context: this.get_context()}
         });
     }
 
@@ -48,7 +92,34 @@ class Model extends BaseModel {
         return super.call({
             method: 'unlink',
             args: [ids],
-            kwargs: { context: {}}
+            kwargs: {context: this.get_context()}
+        });
+    }
+
+    /**
+     * Count all records
+     *
+     * @returns {Promise}
+     */
+    count() {
+        return super.call({
+            method: 'search_count',
+            args: [this._filter],
+            kwargs: {context: this.get_context()}
+        });
+    }
+
+    /**
+     * Fetches the "readable name" for records, based on intrinsic rules
+     *
+     * @param {Array} ids
+     * @returns {Promise}
+     */
+    name_get(ids = []) {
+        return super.call({
+            method: 'name_get',
+            args: [ids],
+            kwargs: {context: this.get_context()}
         });
     }
 }

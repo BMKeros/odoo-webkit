6e059286e68ea8170a07f3f1ac5104f51857162e
Israel Lugo <hostelixisrael@gmail.com>
2018-04-16 16:27:22 -0400

    [REF] refactor in module rpc

--- src/rpc.js
+++ src/rpc.js
@@ -2,13 +2,6 @@ import axios from 'axios';
 
 class Rpc {
     constructor() {
-        this._actions = {
-            dataset_call: '/web/dataset/call',
-            dataset_call_kw: '/web/dataset/call_kw',
-            dataset_search_read: '/web/dataset/search_read',
-            search_read: '/search_read',
-        };
-        
         this._defaults = {
             jsonrpc: '2.0',
             method: 'call'
@@ -18,17 +11,13 @@ class Rpc {
     __jsonrpc(url, params) {
         return axios.post(url, {
             ...this._defaults,
-            id: Math.floor(Math.random() * 1000 * 1000 * 1000),
+            id: this.__generate_id(),
             params: params
         });
     }
 
-    __call(params) {
-        return this.__jsonrpc(this._actions.dataset_call, params);
-    }
-
-    __call_kw(params) {
-        return this.__jsonrpc(this._actions.dataset_call_kw, params);
+    static __generate_id() {
+        return Math.floor(Math.random() * 1000 * 1000 * 1000);
     }
 
     rpc(url, params){

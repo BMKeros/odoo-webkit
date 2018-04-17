d2f8f34c3f775241bc6916d7cae06e2e01aee262
Israel Lugo <hostelixisrael@gmail.com>
2018-04-17 13:40:24 -0400

    [MOD] delete function __generate_id and to adapt to eslint airbnb

--- src/rpc.js
+++ src/rpc.js
@@ -1,48 +1,40 @@
 import axios from 'axios';
 
 class Rpc {
-    constructor() {
-        this._defaults = {
-            jsonrpc: '2.0',
-            method: 'call'
-        };
-    }
+  constructor() {
+    this._defaults = {
+      jsonrpc: '2.0',
+      method: 'call',
+    };
+  }
 
-    /**
-     * Create axios request
-     *
-     * @param {String} url
-     * @param {Object} params
-     *
-     * @returns {Promise}
-     */
-    __jsonrpc(url, params) {
-        return axios.post(url, {
-            ...this._defaults,
-            id: this.__generate_id(),
-            params: params
-        });
-    }
+  /**
+   * Create axios request
+   *
+   * @param {String} url
+   * @param {Object} params
+   *
+   * @returns {Promise}
+   */
+  __jsonrpc(url, params) {
+    return axios.post(url, {
+      ...this._defaults,
+      id: Math.floor(Math.random() * 1000 * 1000 * 1000),
+      params,
+    });
+  }
 
-    /**
-     * Generate unique id
-     * @returns {Number}
-     */
-    static __generate_id() {
-        return Math.floor(Math.random() * 1000 * 1000 * 1000);
-    }
-
-    /**
-     * Create axios request
-     *
-     * @param {String} url
-     * @param {Object} params
-     *
-     * @returns {Promise}
-     */
-    rpc(url, params){
-        return this.__jsonrpc(url, params);
-    }
+  /**
+   * Create axios request
+   *
+   * @param {String} url
+   * @param {Object} params
+   *
+   * @returns {Promise}
+   */
+  rpc(url, params) {
+    return this.__jsonrpc(url, params);
+  }
 }
 
 export default Rpc;

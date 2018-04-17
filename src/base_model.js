7b9afcdc187e6a2e8c851ae829f752f5268fde05
Israel Lugo <hostelixisrael@gmail.com>
2018-04-17 15:26:11 -0400

    [MOD] to adapt eslint airbnb

--- src/base_model.js
+++ src/base_model.js
@@ -1,37 +1,31 @@
 import Rpc from './rpc';
 
 class BaseModel extends Rpc {
-    constructor(name){
-        super();
-        this.name = name;
-    }
+  constructor(name) {
+    super();
+    this.name = name;
+  }
 
-    call({method, args = [], kwargs = {}}){
-        const url = `/web/dataset/call_kw/${this.name}/${method}`;
-        return super.rpc(url, {model: this.name, method, args, kwargs});
-    }
+  call({ method, args = [], kwargs = {} }) {
+    const url = `/web/dataset/call_kw/${this.name}/${method}`;
+    return super.rpc(url, {
+      model: this.name, method, args, kwargs,
+    });
+  }
 
-    search_read({fields = false, domain, context, offset, limit, sort}) {
-        return super.rpc('/web/dataset/search_read', {
-            model: this.name,
-            fields: fields || false,
-            domain: [domain],
-            context: [context],
-            offset: offset,
-            limit: limit,
-            sort: sort
-        });
-    }
+  search_read({
+    fields = false, domain, context, offset, limit, sort,
+  }) {
+    return super.rpc('/web/dataset/search_read', {
+      model: this.name, fields, domain, context, offset, limit, sort,
+    });
+  }
 
-    call_button({method, args = []}) {
-        return super.rpc('/web/dataset/call_button', {
-            model: this.name,
-            method,
-            domain_id: null,
-            //context_id: args.length - 1,
-            args
-        });
-    }
+  call_button({ method, args = [] }) {
+    return super.rpc('/web/dataset/call_button', {
+      model: this.name, method, domain_id: null, args,
+    });
+  }
 }
 
 export default BaseModel;

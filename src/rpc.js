d26ce6896de078d4965c7aa9b999738d1f9cb739
Israel Lugo <hostelixisrael@gmail.com>
2018-04-30 15:17:07 -0400

    [MOD] add method create instance axios

--- src/rpc.js
+++ src/rpc.js
@@ -6,6 +6,24 @@ class Rpc {
       jsonrpc: '2.0',
       method: 'call',
     };
+    this.instanceAxios = this.__makeAxios();
+  }
+
+  /**
+   * Create axios instance
+   *
+   * @returns {Object} [Axios]
+   */
+  __makeAxios() {
+    const instance = axios.create();
+    instance.interceptors.response.use((response) => {
+      if (Object.prototype.hasOwnProperty.call(response.data, 'error')) {
+        return Promise.reject(response.data.error);
+      }
+      return response;
+    }, err => Promise.reject(err));
+
+    return instance;
   }
 
   /**
@@ -17,7 +35,7 @@ class Rpc {
    * @returns {Promise}
    */
   __jsonrpc(url, params) {
-    return axios.post(url, {
+    return this.instanceAxios.post(url, {
       ...this._defaults,
       id: Math.floor(Math.random() * 1000 * 1000 * 1000),
       params,
